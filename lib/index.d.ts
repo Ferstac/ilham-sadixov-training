8b9b8151d42f201b4197f7b98c83bcb5550d01a0
ilham Sadixov <ilham.sadixov@technosec.io>
2021-07-30 23:59:27 +0400

    add index.tsx

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { JupyterFrontEndPlugin } from '@jupyterlab/application';
-/**
- * Initialization data for the jupyterlab_apod extension.
- */
-declare const extension: JupyterFrontEndPlugin<void>;
-export default extension;
--- lib/widgets.d.ts
+++
@@ -1,6 +0,0 @@
-import { JupyterFrontEndPlugin } from '@jupyterlab/application';
-/**
- * Initialization data for the jlab_react extension.
- */
-declare const extension: JupyterFrontEndPlugin<void>;
-export default extension;
